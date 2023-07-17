2af586e011e3d8d30cbfb2a688d28379d19ad7a1
Duncan <duncanwoodpro@gmail.com>
2023-07-17 18:34:31 -0400

    began process to create the backend. Much is left to learn and be done

---
+++ backend/routes/AppRouter.js
@@ -0,0 +1,12 @@
+const Router = require("express").Router();
+// const AuthRouter = require('./AuthRouter')
+// const ResourceRouter = require('./ResourceRouter')
+// const CommentRouter = require('./CommentRouter')
+// const FavoriteRouter = require('./FavoriteRouter')
+
+// Router.use('/auth', AuthRouter)
+// Router.use('/resource', ResourceRouter)
+// Router.use('/comment', CommentRouter)
+// Router.use('/favorite', FavoriteRouter)
+
+module.exports = Router;
